9c6be7f581dcd6f3c6d497a3a3ce7e719fb55a42
NERU0225 <ryouga0225@icloud.com>
2026-07-20 23:06:55 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+/* 家計簿：オフラインでも起動できるようにするためのキャッシュ */
+const CACHE = 'kakeibo-v5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+      .catch(() => {})
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+/* 通信できる時は最新を取りに行き、取れなければキャッシュを返す。
+   これで更新をアップロードすれば次回オンライン起動時に反映される。 */
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET' || !req.url.startsWith('http')) return;
+  e.respondWith(
+    fetch(req)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then(m => m || caches.match('./index.html')))
+  );
+});
