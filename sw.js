b64e7e761e12e3435f53fc97a98bf4a7bf2076ec
NERU0225 <ryouga0225@icloud.com>
2026-08-21 06:39:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    バージョンを上げたら、必ず新しい内容を取り直すこと。
    ブラウザのHTTPキャッシュから古いファイルを拾わないよう cache:'reload' を使う。
 */
-const CACHE = 'kakeibo-v8.1';
+const CACHE = 'kakeibo-v9';
 const REVALIDATE_TIMEOUT = 3000;   // 裏で更新を待つ上限(ms)
 const ASSETS = [
   './',
